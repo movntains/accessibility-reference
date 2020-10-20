3e9789629e5849c49b82f5ed0011e3a2c4be0d3f
Rachel Opperman <oppermanre17@gmail.com>
2020-10-20 16:04:50 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: [],
+  rules: {
+    'arrow-parens': [
+      'warn',
+      'as-needed',
+      {
+        requireForBlockBody: true,
+      },
+    ],
+    'comma-dangle': ['warn', 'always-multiline'],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    quotes: [
+      'warn',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: ['warn', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        named: 'never',
+        anonymous: 'always',
+        asyncArrow: 'always',
+      },
+    ],
+  },
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Accessibility Tips Demo
+
+This repo contains the code for a demo site that will be a companion to a blog post on accessibility tips.
