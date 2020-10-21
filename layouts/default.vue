c0238c0dacdeb766b2f0413e99fdbaf101be2d17
Rachel Opperman <oppermanre17@gmail.com>
2020-10-21 11:47:22 -0400

    Updates default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <AppWrapper>
+    <template #pageContent>
+      <Nuxt />
+    </template>
+  </AppWrapper>
 </template>
+
+<script type="text/babel">
+import { mapState, mapMutations } from 'vuex'
+import AppWrapper from '@/components/global/AppWrapper'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    AppWrapper,
+  },
+  computed: {
+    ...mapState({
+      windowWidth: state => state.ui.windowWidth,
+    }),
+  },
+  watch: {
+    windowWidth() {
+      if (this.windowWidth > 768) {
+        this.resetMobileNav()
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener('resize', this.onResize)
+    window.addEventListener('keydown', this.onFirstTab)
+
+    this.setWindowWidth(window.innerWidth)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize)
+  },
+  methods: {
+    ...mapMutations({
+      setWindowWidth: 'ui/SET_WINDOW_WIDTH',
+      resetMobileNav: 'ui/RESET_MOBILE_NAV',
+    }),
+    onResize(event) {
+      this.setWindowWidth(event.target.innerWidth)
+    },
+    onFirstTab(event) {
+      if (event.keyCode === 9) {
+        document.body.classList.add('using-keyboard')
+
+        window.removeEventListener('keydown', this.onFirstTab)
+      }
+    },
+  },
+}
+</script>
