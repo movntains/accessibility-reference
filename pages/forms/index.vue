40464679da17edd9337da38dccee565765f5cba4
Rachel Opperman <oppermanre17@gmail.com>
2020-10-21 16:46:31 -0400

    Updates forms page

--- pages/forms/index.vue
+++ pages/forms/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    Forms
+  <div class="text-gray-900">
+    <h1
+      class="page-heading"
+      v-text="'Form Tips'"
+    />
+    <FormLabelsSection />
+    <FocusRingsSection />
   </div>
 </template>
 
 <script type="text/babel">
+import FocusRingsSection from '@/components/sections/forms/FocusRingsSection'
+import FormLabelsSection from '@/components/sections/forms/FormLabelsSection'
+
 export default {
   name: 'FormsPage',
+  components: {
+    FocusRingsSection,
+    FormLabelsSection,
+  },
 }
 </script>
