3df61a68a6006c2a4385a92b28012478e6a214d6
Rachel Opperman <oppermanre17@gmail.com>
2020-10-21 12:04:55 -0400

    Adds accessibility plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,9 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [
+    { src: '@/plugins/accessibility-helpers.js', mode: 'client' },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
