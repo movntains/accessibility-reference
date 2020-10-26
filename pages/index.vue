62e380a6fe5b104a30daec48c62dcb36bdb60c9a
Rachel Opperman <oppermanre17@gmail.com>
2020-10-26 08:44:33 -0400

    Updates index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,21 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        a11y-demo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1
+      class="page-heading"
+      v-text="'Accessibility Tips'"
+    />
+    <p
+      class="mb-4"
+      v-text="`Making a website accessible doesn't have to be hard. This site contains accessibility tips, and demonstrations of accessible code vs. inaccessible code. Additional examples will be added over time.`"
+    />
+    <p>
+      To test things out with a screen reader on macOS, you can enable VoiceOver by pressing <code>cmd + fn + F5</code>.
+    </p>
   </div>
 </template>
 
-<script>
-export default {}
+<script type="text/babel">
+export default {
+  name: 'IndexPage',
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
