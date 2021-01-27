162746b10769e65111bd775ccf141b3c04349be1
Rachel Opperman <oppermanre17@gmail.com>
2021-01-27 11:13:57 -0500

    Adds AppSectionWrapper component

---
+++ components/global/AppSectionWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-3xl mx-auto">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script type="text/babel">
+export default {
+  name: 'AppSectionWrapper',
+}
+</script>
