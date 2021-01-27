1ac6219b3d598b8208e8c10d4deeac35dd1abd38
Rachel Opperman <oppermanre17@gmail.com>
2021-01-27 10:25:36 -0500

    Adds favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,14 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Accessibility Tips',
+    title: 'Accessibility Reference',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
   },
 
