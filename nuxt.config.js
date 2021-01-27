04be45251040a975635a467e9a4a9df932395e29
Rachel Opperman <oppermanre17@gmail.com>
2021-01-27 11:37:32 -0500

    Adds vuedraggable package & plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    { src: '@/plugins/vuedraggable.js' },
     { src: '@/plugins/accessibility-helpers.js', mode: 'client' },
   ],
 
