38f0e403a2b26a8f8490d68b6235d6abe98f70b0
Rachel Opperman <oppermanre17@gmail.com>
2021-03-13 17:40:59 -0500

    Updates favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,57 +1,49 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
     title: 'Accessibility Reference',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Tips and tricks for making accessible websites and web apps' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'alternate icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     '@/assets/css/tailwind.scss',
   ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     { src: '@/plugins/vuedraggable.js' },
     { src: '@/plugins/accessibility-helpers.js', mode: 'client' },
   ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [],
 
-  // Tailwind module config
   tailwindcss: {
     exposeConfig: true,
   },
 
-  // Google Fonts package config
   googleFonts: {
     families: {
       Inter: true,
     },
   },
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
